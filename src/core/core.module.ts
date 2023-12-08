9164f99632d0f7e2c88b8d539d65c5adb888c963
Michael <michael@scannable.io>
2023-12-09 08:17:04 +1300

    first commit

---
+++ src/core/core.module.ts
@@ -0,0 +1,28 @@
+import { Module } from '@nestjs/common';
+import { TypeOrmModule } from '@nestjs/typeorm';
+import { ApplicationBootstrapOptions } from '../common/interfaces/application-bootstrap-options.interface';
+
+@Module({})
+export class CoreModule {
+  static forRoot(options: ApplicationBootstrapOptions) {
+    const imports =
+      options.driver === 'orm'
+        ? [
+            TypeOrmModule.forRoot({
+              type: 'postgres',
+              host: 'localhost',
+              port: 5432,
+              password: 'scannableDB123',
+              username: 'scannableDB',
+              autoLoadEntities: true,
+              synchronize: true,
+            }),
+          ]
+        : [];
+
+    return {
+      module: CoreModule,
+      imports,
+    };
+  }
+}
