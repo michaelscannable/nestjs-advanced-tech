ebea7b0eadbc4d100d1f80727c87e4ee1c242b5b
Michael <michael@scannable.io>
2023-12-09 11:48:03 +1300

    Add EventStore interface and implementation

--- src/core/core.module.ts
+++ src/core/core.module.ts
@@ -3,7 +3,14 @@ import { TypeOrmModule } from '@nestjs/typeorm';
 import { ApplicationBootstrapOptions } from '../common/interfaces/application-bootstrap-options.interface';
 import { MongooseModule } from '@nestjs/mongoose';
 import { EVENT_STORE_CONNECTION } from './core.constants';
-@Module({})
+@Module({
+  imports: [
+    MongooseModule.forRoot('mongodb://localhost:27018/vf-event-store', {
+      connectionName: EVENT_STORE_CONNECTION,
+      directConnection: true,
+    }),
+  ],
+})
 export class CoreModule {
   static forRoot(options: ApplicationBootstrapOptions) {
     const imports =
@@ -20,11 +27,6 @@ export class CoreModule {
             }),
 
             MongooseModule.forRoot('mongodb://localhost:27017/vf-read-db'),
-            MongooseModule.forRoot('mongodb://localhost:27018/vf-event-store', {
-              // 👈 from docker-compose
-              connectionName: EVENT_STORE_CONNECTION, // 👈 our new namespace for this mongodb
-              directConnection: true, // 👈 needed needed to connect to local replica sets
-            }),
           ]
         : [];
 
